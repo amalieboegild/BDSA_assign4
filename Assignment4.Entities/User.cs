0b76c0ae3629edcf33d650e0f1743b42dd533ea2
Camille <cgon@itu.dk>
2021-10-04 15:17:37 +0200

    user added entities

--- Assignment4.Entities/User.cs
+++ Assignment4.Entities/User.cs
@@ -2,5 +2,16 @@ namespace Assignment4.Entities
 {
     public class User
     {
+        public int Id{ get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name{ get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Email{ get; set; }
+
+        public ICollection<Task> Tasks { get; set; }
     }
 }
