47831f1b25a9a9daf471c56a3946a985cff57234
Thomas Schauser <thomas.schauser@gmail.com>
2021-10-04 15:18:34 +0200

    Added tag

--- Assignment4.Entities/Tag.cs
+++ Assignment4.Entities/Tag.cs
@@ -1,6 +1,17 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace Assignment4.Entities
 {
     public class Tag
     {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Index(IsUnique=true)]
+        public string Name { get; set; }
+
+        public ICollection<Task> Tasks { get; set; }
     }
 }
