d5171043815cc949c4bc2adb84942b02c15dcf38
AmalieBoegild <abso@itu.dk>
2021-10-04 15:37:23 +0200

    Implement kanbancontext

--- Assignment4.Entities/KanbanContext.cs
+++ Assignment4.Entities/KanbanContext.cs
@@ -1,6 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Assignment4.Core;
 namespace Assignment4.Entities
 {
-    public class KanbanContext
+    public class KanbanContext : DbContext
     {
+         public DbSet<Task> Tasks { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Task>()
+                .Property(e => e.State)
+                .HasConversion(new EnumToStringConverter<State>());
+        }
     }
 }
