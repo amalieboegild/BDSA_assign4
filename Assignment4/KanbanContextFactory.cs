3ef6360cc0f9504c528348b8821684a25ed615bc
Thomas Schauser <thomas.schauser@gmail.com>
2021-10-06 12:59:21 +0200

    work

--- Assignment4/KanbanContextFactory.cs
+++ Assignment4/KanbanContextFactory.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.Extensions.Configuration;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
 using Assignment4.Entities;
 
 namespace Assignment4
@@ -13,55 +13,45 @@ namespace Assignment4
         public KanbanContext CreateDbContext(string[] args)
         {
 
-            var connectionString = "Server=localhost;Database=Kanban;User Id=SA;Password=cb715ee5-3230-43a3-9fc2-adb1ed99d316";
+            var connectionString = "Server=localhost;Database=Kanban;User Id=postgres;Password=1";
 
             var optionsBuilder = new DbContextOptionsBuilder<KanbanContext>()
-                .UseSqlServer(connectionString);
+                .UseNpgsql(connectionString, b => b.MigrationsAssembly("Assignment4"));
 
             return new KanbanContext(optionsBuilder.Options);
         }
 
         public static void Seed(KanbanContext context)
         {
-            Console.WriteLine(context);
-            // context.Database.ExecuteSqlRaw("DELETE dbo.Tasks");
-            // context.Database.ExecuteSqlRaw("DELETE dbo.Tags");
-            // context.Database.ExecuteSqlRaw("DELETE dbo.Users");
-            // context.Database.ExecuteSqlRaw("DELETE dbo.TaskRepositories");
-            // context.Database.ExecuteSqlRaw("DBCC CHECKIDENT ('dbo.Tasks', RESEED, 0)");
-            // context.Database.ExecuteSqlRaw("DBCC CHECKIDENT ('dbo.Tags', RESEED, 0)");
-            // context.Database.ExecuteSqlRaw("DBCC CHECKIDENT ('dbo.Users', RESEED, 0)");
-            // context.Database.ExecuteSqlRaw("DBCC CHECKIDENT ('dbo.TaskRepositories', RESEED, 0)");
+        //     var user = new User {
+        //         Id = 1,
+        //         Name = "Snoop Dog",
+        //         Email = "abc@skrrrt.com",
+        //         Tasks = new List<Task>()
+        //     };
 
-            var user = new User {
-                Id = 1,
-                Name = "Snoop Dog",
-                Email = "abc@skrrrt.com",
-                Tasks = new List<Task>()
-            };
+        //     var tag = new Tag {
+        //       Id = 1,
+        //       Name = "Skrrrt",
+        //       Tasks = new List<Task>()
+        //     };
 
-            var tag = new Tag {
-              Id = 1,
-              Name = "Skrrrt",
-              Tasks = new List<Task>()
-            };
+        //     var task = new Task {
+        //         Id = 1,
+        //         Title = "Work on it",
+        //         AssignedTo = user,
+        //         Description = "Yeeet",
+        //         State = Core.State.Active,
+        //         Tags = new List<Tag> { tag }
+        //     };
 
-            var task = new Task {
-                Id = 1,
-                Title = "Work on it",
-                AssignedTo = user,
-                Description = "Yeeet",
-                State = Core.State.Active,
-                Tags = new List<Tag> { tag }
-            };
+        //     tag.Tasks.Add(task);
 
-            tag.Tasks.Add(task);
+        //     context.Users.AddRange(
+        //         user
+        //     );
 
-            context.Users.AddRange(
-                user
-            );
-
-            context.SaveChanges();
+        //     context.SaveChanges();
         }
     }
 }
