7e553ac5e87303aa7393600cdede50cd0d0a2265
AmalieBoegild <abso@itu.dk>
2021-10-08 13:27:43 +0200

    Make TagRepository class

---
+++ Assignment4.Entities/TagRepository.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using Assignment4.Core;
+using System.Linq;
+
+namespace Assignment4.Entities
+{
+    public class TagRepository
+    {
+        private readonly KanbanContext dbContext;
+
+        public TagRepository(KanbanContext context)
+        {
+            dbContext = context;
+        }
+
+        
+    }
+}
